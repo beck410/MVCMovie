59f137a8802a90d712b8a039696b2c251b491a74
beck410 <beck410@hotmail.com>
2015-03-04 20:16:47 -0600

    configured model data for view

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCMovie.Models {
   public class Movie {
+
     public int ID { get; set; }
     public string Title { get; set; }
+
+    [Display(Name = "Release Year")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
     public string Genre { get; set; }
     public decimal Price { get; set; }
